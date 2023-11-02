baede15e1b73f270284f442bf6123fd2ae7201a4
Clevercodex <115954304+Clevercodx@users.noreply.github.com>
2023-11-02 12:59:42 +0000

    Add files via upload

---
+++ js/send.js
@@ -0,0 +1,1 @@
+document.getElementById('phone').addEventListener('input',function(e){var x=e.target.value.replace(/\D/g,'').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);e.target.value=!x[2]?x[1]:'('+x[1]+') '+x[2]+(x[3]?'-'+x[3]:'');document.getElementById('submit').addEventListener('click',disableButton)});function disableButton(){document.getElementById("overlay").style.display="block";}
